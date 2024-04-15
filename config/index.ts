e7859cae506eda84c0e2e05a89b3a503db22a2d7
wpplumber <mail.tarik.rital@gmail.com>
2024-04-15 18:20:23 +0100

    Make production API BASE URL configurable

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const API_BASE_URL = dev
   ? 'http://localhost:3000'
-  : 'https://flon-nu.vercel.app';
+  : process.env.NEXT_PUBLIC_API_BASE_URL;
